ad26ad137b90d2fc962b0c4a4b75ed5f03ebe8c3
valentin994 <valentin.vareskic@outlook.com>
2022-04-17 15:46:39 +0200

    initial commit

---
+++ src/Components/NavigationComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+NavigationComponent.propTypes = {
+    
+};
+
+function NavigationComponent(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default NavigationComponent;
